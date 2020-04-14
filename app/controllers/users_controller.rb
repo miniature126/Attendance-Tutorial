2bca6fffe58ef20aee4864ffa59dbc5a3639ce9f
R.Nakamura <krw.psh2196@gmail.com>
2020-04-14 11:52:26 +0000

    [5.5.4]登録失敗時のエラーメッセージを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #保存成功時に処理実行
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
