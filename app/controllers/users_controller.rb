c5b504c4d382c25b29a919d704844df0cae90f11
R.Nakamura <krw.psh2196@gmail.com>
2020-04-14 12:04:21 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #保存成功時に処理実行
+      flash[:success] = "新規作成に成功しました。"
+      redirect_to @user
     else
       render :new
     end
