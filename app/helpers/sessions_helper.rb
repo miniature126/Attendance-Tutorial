7072541702b0f1a83524b8ba79a4e701bcc35c95
R.Nakamura <krw.psh2196@gmail.com>
2020-04-19 02:14:06 +0000

    [6.4]ログイン・ログアウトが対応使用のレイアウトに変更

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,4 +11,9 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+  
+  #ログイン中ユーザーがいればtrue、そうでなければfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
