86bbaaf60f732cec611b666530aa8e3eb8c959c6
R.Nakamura <krw.psh2196@gmail.com>
2020-04-20 13:21:34 +0000

    Finish full-fledged-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,8 @@ class User < ApplicationRecord
     
     #トークンがダイジェストと一致すればtrueを返す
     def authenticated?(remember_token)
+        #ダイジェストが存在しない場合はfalseを返して終了する。
+        return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
     
