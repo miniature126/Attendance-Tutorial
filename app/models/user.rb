aea856471b9755d3d8261f9f454df423c89a8a42
R.Nakamura <krw.psh2196@gmail.com>
2020-04-25 06:43:34 +0000

    [9.1.2]ユーザーページに所属の表示を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,9 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 100 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: true
+    validates :department, length: { in: 2..30 }, allow_blank: true
     has_secure_password
+    #[allow_nil: true]→編集時にパスワードを入力していない場合は検証をスルー。
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
     
     #渡された文字列のハッシュ値を返す
