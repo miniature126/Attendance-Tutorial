9e20977c56974d3070f3655e38fe06887b5b30eb
R.Nakamura <krw.psh2196@gmail.com>
2020-04-26 05:49:31 +0000

    [9.2.1]勤務時間に関するカラムを作成、バリデーションを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
   end
   
   #beforeフィルター
@@ -77,4 +77,3 @@ class UsersController < ApplicationController
     redirect_to root_url unless current_user.admin?
   end
 end
-
