f402a520e32f7aef87056e8869b88322c4a35a95
R.Nakamura <krw.psh2196@gmail.com>
2020-04-29 03:00:14 +0000

    [10.5.3]出勤登録アクションを作成

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,16 @@
 class AttendancesController < ApplicationController
+  
+  def update
+    @user = User.find(params[:user_id])
+    @attendance = Attendance.find(params[:id])
+    #出勤時間が未登録であることを判定する。
+    if @attendance.started_at.nil? #出勤時間入力されてますか？
+      if @attendance.update_attributes(started_at: Time.current.change(sec: 0)) #入力されてなかったら現在時刻を入れる。
+        flash[:info] = "おはようございます！"
+      else
+        flash[:danger] = "勤怠登録に失敗しました。やり直してください。"
+      end
+    end
+    redirect_to @user
+  end
 end
